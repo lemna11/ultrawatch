78d6a85eb3f7d0f13cb2c7923829632ea67d99f1
Mano <lemmensmano@gmail.com>
2025-11-27 01:36:33 +0100

    change resource files

--- Weapons/RocketLauncher/RocketLauncher.cs
+++ Weapons/RocketLauncher/RocketLauncher.cs
@@ -1,22 +1,22 @@
 public partial class RocketLauncher : Node3D, IWeapon {
-    [Export]
-    public PackedScene rocket_scene;
+	[Export]
+	public PackedScene rocket_scene;
 
-    [Export]
-    public float fire_rate = 1.0f;
+	[Export]
+	public float fire_rate = 1.0f;
 
-    public async void Shoot(WeaponResource weapon, Player _) {
-        var rocket_spawn_point = weapon.player_camera;
-        var rocket_instance = rocket_scene.Instantiate<Rocket>();
-        rocket_instance.weapon = weapon;
-        rocket_spawn_point.AddChild(rocket_instance);
-        await ToSignal(GetTree(), "process_frame");
-        rocket_instance.Reparent(GetTree().CurrentScene);
-    }
+	public async void Shoot(WeaponResource weapon, Player _) {
+		var rocket_spawn_point = weapon.player_camera;
+		var rocket_instance = rocket_scene.Instantiate<Rocket>();
+		rocket_instance.weapon = weapon;
+		rocket_spawn_point.AddChild(rocket_instance);
+		await ToSignal(GetTree(), "process_frame");
+		rocket_instance.Reparent(GetTree().CurrentScene);
+	}
 
-    public void Equip(WeaponResource weapon) {
-    }
+	public void Equip(WeaponResource weapon) {
+	}
 
-    public void Unequip(WeaponResource weapon) {
-    }
+	public void Unequip(WeaponResource weapon) {
+	}
 }
