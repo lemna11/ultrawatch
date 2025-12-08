3f02670f4a61fa9c9ddb2799a3f292d365a7b34b
Martin <martin.sitta@alumni.fh-aachen.de>
2025-12-08 15:48:00 +0100

    Replaced player.cs
    Replaced the file due to a improper rebase

--- Player.cs
+++ Player.cs
@@ -13,192 +13,102 @@ public partial class Player : CharacterBody3D, ITarget {
         WallJump
     }
 
-	[Export]
-	public Node3D camera_yaw;
-
-	[Export]
-	public Node3D camera_pitch;
-
-	[Export]
-	public float move_speed = 6.0f;
-
-	[Export]
-	public float crouch_speed = 3.0f;
+    [Export]
+    public Node3D camera_yaw;
 
     [Export]
-    public float crouch_slide_velocity = 12.0f;
+    public Node3D camera_pitch;
 
     [Export]
-    public float crouch_slide_accel = 18.0f;
+    public float move_speed = 6.0f;
 
     [Export]
-    public float universal_deccel = 12.0f;
+    public float crouch_speed = 3.0f;
 
     [Export]
-    public float jump_velocity = 4.5f;
+    public float universal_deccel = 16.0f;
+
+    [Export]
+    public float universal_accel = 12.0f;
 
     [Export]
     public float jump_vert_vel = 4.5f;
 
     [Export]
-    public float jump_hor_accel = 12.0f;
+    public float mouse_sensitivity = 3.0f;
 
-	[Export]
-	public float mouse_sensitivity = 3.0f;
+    [Export]
+    public int max_health = 100;
 
-	[Export]
-	public int max_health = 100;
+    [Export]
+    public LegType leg_type;
 
-	[Export]
-	public LegType leg_type;
+    public int cur_health;
 
-	public int cur_health;
-
-	public Action update_hud;
+    public Action update_hud;
 
     private ILegAbility current_legs;
 
-    public static float HorLenHelper(Vector3 input) {
-        return Mathf.Sqrt(input.X * input.X + input.Z * input.Z);
-    }
-
-    public static Vector3 KillMomentumProportionalHelper(Vector3 velocity_vector, double deccel) {
-        var vect_len = HorLenHelper(velocity_vector);
-        float new_vect_len = (float)Mathf.MoveToward(vect_len, 0, deccel);
-        var vect_normal = HorNormalHelper(velocity_vector);
-        var new_velocity = vect_normal * new_vect_len;
-        new_velocity.Y = velocity_vector.Y;
-        return new_velocity;
-    }
-
-    public static Vector3 KillMomentumOnAxisHelper(Vector3 velocity_vector, Vector3 orientation, double deccel) {
-        var vect_len = HorLenHelper(velocity_vector);
-        Vector3 new_vect = velocity_vector;
-        Vector3 return_vect = Vector3.Zero;
-        double deccel_vel_x = (1 - Mathf.Abs(orientation.X)) * deccel;
-        double deccel_vel_z = (1 - Mathf.Abs(orientation.Z)) * deccel;
-        new_vect.X = (float)Mathf.MoveToward(velocity_vector.X, 0, deccel_vel_x);
-        new_vect.Z = (float)Mathf.MoveToward(velocity_vector.Z, 0, deccel_vel_z);
-        Vector3 new_vect_normal = HorNormalHelper(new_vect);
-        float return_vect_scalar = (float)Mathf.MoveToward(vect_len, 0, deccel);
-        return_vect.X = new_vect_normal.X * return_vect_scalar;
-        return_vect.Z = new_vect_normal.Z * return_vect_scalar;
-        return_vect.Y = velocity_vector.Y;
-        return return_vect;
-    }
-
-    public static Vector3 RescaleVector1ToVector2Helper(Vector3 vect1, Vector3 vect2) {
-        Vector2 relevant_dimensions1 = Vector2.Zero;
-        relevant_dimensions1.X = vect1.X;
-        relevant_dimensions1.Y = vect1.Z;
-        Vector2 relevant_dimensions2 = Vector2.Zero;
-        relevant_dimensions2.X = vect2.X;
-        relevant_dimensions2.Y = vect2.Z;
-        Vector2 scaled_relevant_dimensions = relevant_dimensions1.Normalized() * relevant_dimensions2.Length();
-        Vector3 out_vect = Vector3.Zero;
-        out_vect.X = scaled_relevant_dimensions.X;
-        out_vect.Y = vect1.Y;
-        out_vect.Z = scaled_relevant_dimensions.Y;
-        return out_vect;
-    }
-
-    public static Vector3 HorNormalHelper(Vector3 input) {
-        Vector2 hor_vect = Vector2.Zero;
-        hor_vect.X = input.X;
-        hor_vect.Y = input.Z;
-        hor_vect = hor_vect.Normalized();
-        Vector3 output = Vector3.Zero;
-        output.X = hor_vect.X;
-        output.Z = hor_vect.Y;
-        return output;
-    }
-
-    private static bool IsCounterStrafingHelper(Vector3 orientation, Vector3 velocity) {
-        if (velocity == Vector3.Zero) {
-            return false;
-        }
-        Vector3 diff_vect = orientation - HorNormalHelper(velocity);
-        double angle = Mathf.Asin(HorLenHelper(diff_vect) / 2) * 2;
-        if (Mathf.Abs(angle) <= Mathf.Pi / 2) {
-            return false;
-        }
-        return true;
-    }
-
-    public static Vector3 UniAccelDeccelHandler(Vector3 velocity, Vector3 direction, float accel, float deccel, double delta, float max_spd) {
-        if (direction == Vector3.Zero)
-            velocity = KillMomentumProportionalHelper(velocity, deccel * delta);
-        else {
-            velocity += direction * accel * (float)delta;
-            if (HorLenHelper(velocity) > max_spd) {
-                velocity = KillMomentumProportionalHelper(velocity, deccel * delta);
-            }
-        }
-        return velocity;
-    }
-
     public override void _Ready() {
         Input.MouseMode = Input.MouseModeEnum.Captured;
-        cur_health = max_health;
         current_legs = leg_type switch {
-            LegType.AirStomp => new AirStomp(),
-            LegType.ChargeJump => new ChargeJump(),
+            LegType.Default => new DefaultLegs(),
+            LegType.StompAndHighJump => new StompAndJump(),
             LegType.DoubleJump => new DoubleJump(),
             LegType.HoverJet => new HoverJet(),
             LegType.WallJump => new WallJump(),
             _ => throw new InvalidEnumArgumentException()
         };
+        max_health += current_legs.MaxHealthModifier;
+        cur_health = max_health;
+        update_hud();
     }
 
-	public override void _Process(double delta) {
-		base._Process(delta);
-	}
+    public override void _Process(double delta) {
+        base._Process(delta);
+    }
 
-	public override void _PhysicsProcess(double delta) {
-		HandleMovement(delta);
-	}
+    public override void _PhysicsProcess(double delta) {
+        HandleMovement(delta);
+    }
 
-	public override void _Input(InputEvent @event) {
-		if (@event is InputEventMouseMotion mouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured) {
-			// default godot sens is absurdly high, so we scale it down
-			camera_yaw.RotateY(-mouseMotion.Relative.X * mouse_sensitivity * 0.00166666667f);
-			camera_pitch.RotateX(-mouseMotion.Relative.Y * mouse_sensitivity * 0.00166666667f);
+    public override void _Input(InputEvent @event) {
+        if (@event is InputEventMouseMotion mouseMotion && Input.MouseMode == Input.MouseModeEnum.Captured) {
+            // default godot sens is absurdly high, so we scale it down
+            camera_yaw.RotateY(-mouseMotion.Relative.X * mouse_sensitivity * 0.00166666667f);
+            camera_pitch.RotateX(-mouseMotion.Relative.Y * mouse_sensitivity * 0.00166666667f);
 
-			camera_pitch.RotationDegrees = new Vector3(
-				Mathf.Clamp(camera_pitch.RotationDegrees.X, -90, 90),
-				camera_pitch.RotationDegrees.Y,
-				camera_pitch.RotationDegrees.Z
-			);
-		}
-		// quick hack to toggle mouse capture
-		else if (@event is InputEventKey keyEvent && keyEvent.Pressed && keyEvent.Keycode == Key.Escape) {
-			if (Input.MouseMode == Input.MouseModeEnum.Captured)
-				Input.MouseMode = Input.MouseModeEnum.Visible;
-			else
-				Input.MouseMode = Input.MouseModeEnum.Captured;
-		} else
-			base._Input(@event);
-	}
+            camera_pitch.RotationDegrees = new Vector3(
+                Mathf.Clamp(camera_pitch.RotationDegrees.X, -90, 90),
+                camera_pitch.RotationDegrees.Y,
+                camera_pitch.RotationDegrees.Z
+            );
+        }
+        // quick hack to toggle mouse capture
+        else if (@event is InputEventKey keyEvent && keyEvent.Pressed && keyEvent.Keycode == Key.Escape) {
+            if (Input.MouseMode == Input.MouseModeEnum.Captured)
+                Input.MouseMode = Input.MouseModeEnum.Visible;
+            else
+                Input.MouseMode = Input.MouseModeEnum.Captured;
+        } else
+            base._Input(@event);
+    }
 
     private void HandleMovement(double delta) {
         Vector3 direction = Vector3.Zero;
         Basis cameraBasis = camera_yaw.GlobalTransform.Basis;
-        bool input_foward = Input.IsActionPressed("move_forward");
-        bool input_back = Input.IsActionPressed("move_back");
-        bool input_left = Input.IsActionPressed("move_left");
-        bool input_right = Input.IsActionPressed("move_right");
-        var any_input = input_foward || input_back || input_left || input_right;
-        var no_input = !any_input;
         var velocity = Velocity;
-        if (input_foward)
+        if (Input.IsActionPressed("move_forward"))
             direction -= cameraBasis.Z;
-        if (input_back)
+        if (Input.IsActionPressed("move_back"))
             direction += cameraBasis.Z;
-        if (input_left)
+        if (Input.IsActionPressed("move_left"))
             direction -= cameraBasis.X;
-        if (input_right)
+        if (Input.IsActionPressed("move_right"))
             direction += cameraBasis.X;
-        direction = HorNormalHelper(direction);
+        direction = HorizontalNormal(direction);
+        var speed = Input.IsActionPressed("move_crouch") ? crouch_speed : move_speed;
+
         //apply gravity
         if (!IsOnFloor()) {
             Vector3 gravity = GetGravity();
@@ -209,38 +119,20 @@ public partial class Player : CharacterBody3D, ITarget {
             velocity.Y = jump_vert_vel;
         }
 
-        if (no_input && IsOnFloor()) {//no input on floor
-            if (HorLenHelper(velocity) > move_speed) {
-                velocity = UniAccelDeccelHandler(velocity, direction, 0, universal_deccel, delta, move_speed);
-            } else {
-                velocity.X = 0;
-                velocity.Z = 0;
-            }
-        } else if (any_input && IsOnFloor()) {//input on floor
-            if (HorLenHelper(velocity) > move_speed) {
-                velocity = UniAccelDeccelHandler(velocity, direction, 0, universal_deccel, delta, move_speed);
-            } else {
-                if (Input.IsActionPressed("move_crouch")) {
-                    velocity.X = direction.X * crouch_speed;
-                    velocity.Z = direction.Z * crouch_speed;
-                } else {
-                    velocity.X = direction.X * move_speed;
-                    velocity.Z = direction.Z * move_speed;
-                }
-            }
-        } else if (no_input && !IsOnFloor()) {//no input when airborne		
-            velocity = KillMomentumProportionalHelper(velocity, universal_deccel * delta);
-        } else if (any_input && !IsOnFloor()) {//input when airborne
-            velocity = UniAccelDeccelHandler(velocity, direction, jump_hor_accel, universal_deccel, delta, move_speed);
+        if (IsOnFloor() && HorizontalLenght(velocity) <= move_speed) {//input on floor
+            velocity.X = direction.X * speed;
+            velocity.Z = direction.Z * speed;
+        } else {
+            velocity = UniAccelDeccelHandler(velocity, direction, universal_accel, universal_deccel, delta, move_speed);
         }
 
-        current_legs.Apply(this, delta, ref velocity);
+        current_legs.Apply(this, delta, direction, ref velocity);
         Velocity = velocity;
         MoveAndSlide();
     }
 
-	public void TakeDamage(WeaponResource weapon) {
-		cur_health = 0 > cur_health - weapon.damage ? 0 : (int)(cur_health - weapon.damage);
-		update_hud();
-	}
+    public void TakeDamage(WeaponResource weapon) {
+        cur_health = 0 > cur_health - weapon.damage ? 0 : (int)(cur_health - weapon.damage);
+        update_hud();
+    }
 }
