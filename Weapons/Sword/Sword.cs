f6dc3df8172011f4c0620acaf481af1b85ea405a
Martin <martin.sitta@alumni.fh-aachen.de>
2025-12-19 02:30:40 +0100

    tweaked the sword logic to give you a fixed foward velocity in testing this made it much MUCH more intuitive with reasonable yank strengths

--- Weapons/Sword/Sword.cs
+++ Weapons/Sword/Sword.cs
@@ -1,55 +1,55 @@
 using System.Collections.Generic;
 
 public partial class Sword : Node3D, IWeapon {
-    [Export]
-    public float YankStrength = 30.0f;
+	[Export]
+	public float YankStrength = 20.0f;
 
-    [Export]
-    public Area3D Hitbox;
+	[Export]
+	public Area3D Hitbox;
 
-    private WeaponResource _currentWeapon;
-    private Player _ownerPlayer;
-    private readonly HashSet<ITarget> _hitTargets = [];
+	private WeaponResource _currentWeapon;
+	private Player _ownerPlayer;
+	private readonly HashSet<ITarget> _hitTargets = [];
 
-    public override void _Ready() {
-        if (Hitbox != null) {
-            Hitbox.BodyEntered += OnBodyEntered;
-            Hitbox.Monitoring = false;
-        }
-    }
+	public override void _Ready() {
+		if (Hitbox != null) {
+			Hitbox.BodyEntered += OnBodyEntered;
+			Hitbox.Monitoring = false;
+		}
+	}
 
-    public void Shoot(WeaponResource weapon, Player player) {
-        _currentWeapon = weapon;
-        _ownerPlayer = player;
+	public void Shoot(WeaponResource weapon, Player player) {
+		_currentWeapon = weapon;
+		_ownerPlayer = player;
 
-        var camera = weapon.player_camera;
-        if (camera != null) {
-            Vector3 forward = -camera.GlobalTransform.Basis.Z;
-            player.Velocity += forward * YankStrength;
-        }
+		var camera = weapon.player_camera;
+		if (camera != null) {
+			Vector3 forward = -camera.GlobalTransform.Basis.Z;
+			player.Velocity = forward * YankStrength;
+		}
 
-        if (Hitbox != null) {
-            Hitbox.Monitoring = true;
-            GetTree().CreateTimer(weapon.fire_rate).Timeout += () => {
-                if (IsInstanceValid(Hitbox)) {
-                    Hitbox.Monitoring = false;
-                }
-                _hitTargets.Clear();
-            };
-        }
-    }
+		if (Hitbox != null) {
+			Hitbox.Monitoring = true;
+			GetTree().CreateTimer(weapon.fire_rate).Timeout += () => {
+				if (IsInstanceValid(Hitbox)) {
+					Hitbox.Monitoring = false;
+				}
+				_hitTargets.Clear();
+			};
+		}
+	}
 
-    private void OnBodyEntered(Node body) {
-        if (body == _ownerPlayer) return;
+	private void OnBodyEntered(Node body) {
+		if (body == _ownerPlayer) return;
 
-        if (body is ITarget target && _hitTargets.Add(target) && _currentWeapon != null) {
-            target.TakeDamage(_currentWeapon);
-        }
-    }
+		if (body is ITarget target && _hitTargets.Add(target) && _currentWeapon != null) {
+			target.TakeDamage(_currentWeapon);
+		}
+	}
 
-    public void Equip(WeaponResource weapon) {
-    }
+	public void Equip(WeaponResource weapon) {
+	}
 
-    public void Unequip(WeaponResource weapon) {
-    }
+	public void Unequip(WeaponResource weapon) {
+	}
 }
