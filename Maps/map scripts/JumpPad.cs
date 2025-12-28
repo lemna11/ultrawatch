36c3b1d355bf4052a51f5b35df7e3439c24f8474
Mano Lemmens <lemmensmano@gmail.com>
2025-12-29 00:28:23 +0100

    FIX: formatting

--- Maps/map scripts/JumpPad.cs
+++ Maps/map scripts/JumpPad.cs
@@ -1,16 +1,16 @@
 namespace Maps;
 
 public partial class JumpPad : Area3D {
-	[Export]
-	public Vector3 launch_velocity = new Vector3(0, 10, 0);
+    [Export]
+    public Vector3 launch_velocity = new Vector3(0, 10, 0);
 
-	public override void _Ready() {
-		BodyEntered += OnBodyEntered;
-	}
+    public override void _Ready() {
+        BodyEntered += OnBodyEntered;
+    }
 
-	private void OnBodyEntered(Node3D body) {
-		if (body is CharacterBody3D characterBody) {
-			characterBody.Velocity = launch_velocity;
-		}
-	}
+    private void OnBodyEntered(Node3D body) {
+        if (body is CharacterBody3D characterBody) {
+            characterBody.Velocity = launch_velocity;
+        }
+    }
 }
