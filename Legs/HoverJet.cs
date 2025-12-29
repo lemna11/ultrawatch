8f2dec70174345d3d18bc5b9be7c5fae04491abe
Mano <lemmensmano@gmail.com>
2025-12-29 01:05:01 +0100

    FIX: formatting pt2, electric boogaloo

--- Legs/HoverJet.cs
+++ Legs/HoverJet.cs
@@ -3,15 +3,15 @@ namespace Legs;
 using static Utils.Util;
 
 public class HoverJet : ILegAbility {
-	private readonly float move_speed = 12.0f;
+    private readonly float move_speed = 12.0f;
 
-	public int MaxHealthModifier => 25;
+    public int MaxHealthModifier => 25;
 
-	public void Apply(Player player, double delta, Vector3 direction, ref Vector3 velocity) {
-		if (player.IsOnFloor()) {
-			velocity.X = player.Velocity.X;
-			velocity.Z = player.Velocity.Z;
-		}
-		velocity = UniAccelDeccelHandler(velocity, direction, player.universal_accel, player.universal_deccel, delta, move_speed);
-	}
+    public void Apply(Player player, double delta, Vector3 direction, ref Vector3 velocity) {
+        if (player.IsOnFloor()) {
+            velocity.X = player.Velocity.X;
+            velocity.Z = player.Velocity.Z;
+        }
+        velocity = UniAccelDeccelHandler(velocity, direction, player.universal_accel, player.universal_deccel, delta, move_speed);
+    }
 }
