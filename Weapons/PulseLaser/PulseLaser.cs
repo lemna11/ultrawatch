ee16787e22b43d5c726cfb43f1da4944a1ef479a
Martin <martin.sitta@alumni.fh-aachen.de>
2026-08-25 02:29:52 +0200

    Partial implementation of shoot function
    Partially implemented the shoot function to set up the groundwork on the raycasting. next up is processing the results.

--- Weapons/PulseLaser/PulseLaser.cs
+++ Weapons/PulseLaser/PulseLaser.cs
@@ -6,6 +6,7 @@ public partial class PulseLaser : Node3D, IWeapon{
     [Export]
     float PulseLaserWindupLength = 2.5f;
     float CurrentWindupLength = 0.0f;
+    int RayLength = 100;
     //This is intended to behave similar to hanzos bow in overwatch but as a hitscan
     //key difference is if the player doesnt fully charge the shot, the shot doesnt happen at all 
     //the the current Windup length is reset
@@ -28,5 +29,22 @@ public partial class PulseLaser : Node3D, IWeapon{
         //shoot a ray with a infinite range at the current point of aim
         //make shooting animation happen the ray will determine the length
         //if the ray hits a player, the player takes dmg
+
+        //THIS IS UNTESTED CODE! The code is taken from this: https://docs.godotengine.org/en/stable/tutorials/physics/ray-casting.html
+        //To my knowledge now I need to iterate over the results and find the collision entry
+        //with the lowest euclidean distance from the player
+        //And I also need to exclude the players own hitbox
+        //since the docs said that I need to explicitly remove that
+
+        var SpaceState = GetWorld3D().DirectSpaceState;
+        Camera3D Cam = player.GetNode<Camera3D>("Camera3D");
+        Vector2 MousePosition = player.GetViewport().GetMousePosition();
+        Vector3 Origin = Cam.ProjectRayOrigin(MousePosition);
+        Vector3 End = Origin + Cam.ProjectRayNormal(MousePosition) * RayLength;
+        var Query = PhysicsRayQueryParameters3D.Create(Origin, End, player.CollisionMask);
+        var Result = SpaceState.IntersectRay(Query);
+        
+            
+        
     }
 }
